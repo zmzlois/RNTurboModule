f2248fa52948967bf06b51ca6528c2cc3bd83290
zmzlois <lois.z@comcord.vision>
2024-12-23 22:16:34 +0000

    bind native platform with kotlin and swift

--- App.tsx
+++ App.tsx
@@ -9,13 +9,16 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
+
+    TextInput,
+  Button,
   StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
+
+
 
 import {
   Colors,
@@ -24,6 +27,11 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+
+import NativeLocalStorage from './specs/NativeLocalStorage';
+
+const EMPTY = '<empty>';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -56,43 +64,46 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [value, setValue] = React.useState<string | null>(null);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [editingValue, setEditingValue] = React.useState<
+      string | null
+  >(null);
+
+  React.useEffect(() => {
+    const storedValue = NativeLocalStorage?.getItem('myKey');
+    setValue(storedValue ?? '');
+  }, []);
+
+  function saveValue() {
+    NativeLocalStorage?.setItem(editingValue ?? EMPTY, 'myKey');
+    setValue(editingValue);
+  }
+
+  function clearAll() {
+    NativeLocalStorage?.clear();
+    setValue('');
+  }
+
+  function deleteValue() {
+    NativeLocalStorage?.removeItem(editingValue ?? EMPTY);
+    setValue('');
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <SafeAreaView style={{flex: 1}}>
+        <Text style={styles.text}>
+          Current stored value is: {value ?? 'No Value'}
+        </Text>
+        <TextInput
+            placeholder="Enter the text you want to store"
+            style={styles.textInput}
+            onChangeText={setEditingValue}
+        />
+        <Button title="Save" onPress={saveValue} />
+        <Button title="Delete" onPress={deleteValue} />
+        <Button title="Clear" onPress={clearAll} />
+      </SafeAreaView>
   );
 }
 
@@ -112,6 +123,19 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontWeight: '700',
+  },
+  text: {
+    margin: 10,
+    fontSize: 20,
+  },
+  textInput: {
+    margin: 10,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 5,
   },
 });
 
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  RNTurboModule
+//
+//  Created by Lois Zhao on 23/12/2024.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject<NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
